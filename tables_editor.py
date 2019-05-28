f062b0649e6d42f04f52b51bebe2cd76213d4b68
Wayne C. Gramlich <Wayne@Gramlich.Net>
2019-05-28 14:33:59 -0700

    Merged code from node.py, model.py, and lazyload.py into tables_editor.py.

--- tables_editor.py
+++ tables_editor.py
@@ -52,7 +52,7 @@ from PySide2.QtWidgets import (QApplication, QComboBox, QLineEdit, QMainWindow,
                                QTreeView, QFileSystemModel,
                                QTreeWidget, QTreeWidgetItem,
                                QWidget)
-from PySide2.QtCore import (QAbstractItemModel, QDir, QFile, QItemSelectionModel, Qt)
+from PySide2.QtCore import (QAbstractItemModel, QDir, QFile, QItemSelectionModel, QModelIndex, Qt)
 
 class ComboEdit:
     """ A *ComboEdit* object repesents the GUI controls for manuipulating a combo box widget.
@@ -982,6 +982,75 @@ class Filter:
         if tracing is not None:
             print("{0}<=Filter.xml_lines_append()".format(tracing))
 
+
+class Node(object):
+    """ Represents a single *Node* in a *QTreeView* tree. """
+
+    def __init__(self, name, path, parent=None):
+        # Verify argument types:
+        assert isinstance(name, str)
+        assert isinstance(path, str)
+        assert isinstance(parent, Node) or parent is None
+
+        # Initilize the parent type of *node* (i.e. *self*):
+        node = self
+        super(Node, self).__init__()
+
+        is_dir = os.path.isdir(path)
+        is_traversed = not is_dir or is_dir and len(list(os.listdir(path))) == 0
+
+        # Load up *node* (i.e. *self*):
+        node.children = []
+        node.name = name
+        node.is_dir = is_dir
+        node.is_traversed = is_traversed
+        node.parent = parent
+        node.path = path
+
+        # Force *node* to be in *parent*:
+        if parent is not None:
+            parent.add_child(node)
+
+    def add_child(self, child):
+        # Verify argument types:
+        assert isinstance(child, Node)
+
+        # Append *child* to the *node* (i.e. *self*) children list:
+        node = self
+        node.children.append(child)
+        child.parent = node
+
+    def insert_child(self, position, child):
+        # Verify argument types:
+        assert isinstance(position, int)
+        assert isinstance(child, Node)
+    
+        node = self
+        children = node.children
+        inserted = 0 <= position <= len(children)
+        if inserted:
+            children.insert(position, child)
+            child.parent = node
+        return inserted
+
+    def child(self, row):
+        # Verify argument types:
+        assert isinstance(row, int)
+
+        node = self
+        children = node.children
+        result = children[row] if 0 <= row < len(children) else None
+        return result
+
+    def child_count(self):
+        node = self
+        return len(node.children)
+
+    def row(self):
+        node = self
+        parent = node.parent
+        result = 0 if parent is None else parent.children.index(node)
+        return result
 
 class Parameter:
 
@@ -1958,23 +2027,28 @@ class TablesEditor(QMainWindow):
             print("*****************************************************")
             path = "/home/wayne/public_html/projects/digikey_tables"
 
-            if True:
+            if False:
                 file_system_model = QFileSystemModel()
                 assert isinstance(file_system_model, QFileSystemModel)
                 assert isinstance(file_system_model, QAbstractItemModel)
                 file_system_model.setRootPath((QDir.rootPath()))
                 model = file_system_model    
             else:
-                tree_object_model = TreeObjectModel()
-                assert isinstance(tree_object_model, TreeObjectModel)
-                assert isinstance(tree_object_model, QAbstractItemModel)
-                model = tree_object_model
+                path="/home/wayne/public_html/projects/digikey_tables"
+                #path="/tmp/digikey"
+                root_node = Node("Root", path)
+                assert root_node.is_dir
+                model = TreeModel(root_node, path)
 
+                #tree_object_model = TreeModel()
+                #assert isinstance(tree_object_model, TreeObjectModel)
+                #assert isinstance(tree_object_model, QAbstractItemModel)
+                #model = tree_object_model
             tables_editor.model = model
 
             print("module=", model)
             schema_tree.setModel(model)
-            schema_tree.setRootIndex(model.index(path))
+            #schema_tree.setRootIndex(model.index(path))
             schema_tree.setSortingEnabled(True)
         elif isinstance(schema_tree, QTreeWidget):
             schema_tree.setColumnCount(2)
@@ -4201,6 +4275,171 @@ class TablesEditor(QMainWindow):
         if tracing is not None:
             print("{0}<=TablesEditor.search_update(*)".format(tracing))
 
+
+class TreeModel(QAbstractItemModel):
+
+    FLAG_DEFAULT = Qt.ItemIsEnabled | Qt.ItemIsSelectable
+
+    def __init__(self, root_node, path):
+        # Verify argument types:
+        assert isinstance(root_node, Node)
+        assert root_node.is_dir
+        assert isinstance(path, str)
+
+        # Initialize the parent *QAbstraceItemModel*:
+        super(TreeModel, self).__init__()
+
+        # Stuff *root* into *model* (i.e. *self*):
+        model = self
+        model.headers = {0: "Name", 1: "Type"}
+        model.path = path
+        model.root_node = root_node
+
+        # Populate the top level of *root_node*:
+        file_names = sorted(os.listdir(path))
+        for file in file_names:
+            file_path = os.path.join(path, file)
+            node = Node(file, file_path, parent=root_node)
+        root_node.is_traversed = True
+
+    # takes a model index and returns the related Python node
+    def getNode(self, index):
+        # Verify argument types:
+        assert isinstance(index, QModelIndex)
+
+        model = self
+        node = index.internalPointer() if index.isValid() else model.root_node
+        assert isinstance(node, Node)
+        return node
+
+    # check if the note has data that has not been loaded yet
+    def canFetchMore(self, index):
+        # Verify argument types:
+        assert isinstance(index, QModelIndex)
+
+        model = self
+        node = model.getNode(index)
+        can_fetch_more = node.is_dir and not node.is_traversed
+        return can_fetch_more
+
+    # called if canFetchMore returns True, then dynamically inserts nodes required for
+    # directory contents
+    def fetchMore(self, index):
+        # Verify argument types:
+        assert isinstance(index, QModelIndex)
+
+        model = self
+        parent = model.getNode(index)
+
+        nodes = []
+        for file in sorted(os.listdir(parent.path)):
+            file_path = os.path.join(parent.path, file)
+            node = Node(file, file_path)
+            nodes.append(node)
+
+        model.insertNodes(0, nodes, index)
+        parent.is_traversed = True
+
+    # returns True for directory nodes so that Qt knows to check if there is more to load
+    def hasChildren(self, index):
+        # Verify argument types:
+        assert isinstance(index, QModelIndex)
+
+        model = self
+        node = model.getNode(index)
+        has_children = ((node.is_dir and not node.is_traversed) or
+          super(TreeModel, model).hasChildren(index))
+        return has_children
+
+    # Return 0 if there is data to fetch (handled implicitly by check length of child list)
+    def rowCount(self, parent):
+        # Verify argument types:
+        assert isinstance(parent, QModelIndex)
+        model = self
+        node = model.getNode(parent)
+        return node.child_count()
+
+    def columnCount(self, parent):
+        # Verify argument types:
+        assert isinstance(parent, QModelIndex)
+        return 2
+
+    def flags(self, index):
+        # Verify argument types:
+        assert isinstance(index, QModelIndex)
+        return TreeModel.FLAG_DEFAULT
+
+    def parent(self, index):
+        # Verify argument types:
+        assert isinstance(index, QModelIndex)
+
+        model = self
+        node = model.getNode(index)
+
+        parent = node.parent
+        index = (QModelIndex() if parent == model.root_node else
+                 model.createIndex(parent.row(), 0, parent))
+        assert isinstance(index, QModelIndex)
+        return index
+
+    def index(self, row, column, parent):
+        # Verify argument types:
+        assert isinstance(row, int)
+        assert isinstance(column, int)
+        assert isinstance(parent, QModelIndex)
+
+        model = self
+        node = model.getNode(parent)
+        child = node.child(row)
+        index = QModelIndex() if child is None else model.createIndex(row, column, child)
+        assert isinstance(index, QModelIndex)
+        return index
+
+    def headerData(self, section, orientation, role):
+        assert isinstance(section, int)
+        assert isinstance(orientation, Qt.Orientation)
+        assert isinstance(role, int)
+
+        model = self
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return model.headers[section]
+        return None
+
+    def data(self, index, role):
+        # Verify argument types:
+        assert isinstance(index, QModelIndex)
+        assert isinstance(role, int)
+
+        column = index.column()
+        value = None
+        if index.isValid():
+            node = index.internalPointer()
+            if role == Qt.DisplayRole:
+                if column == 0:
+                    value = node.name
+                elif column == 1:
+                    value = "D" if node.is_dir else "T"
+        assert isinstance(value, str) or value is None
+        return value
+
+    def insertNodes(self, position, nodes, parent=QModelIndex()):
+        assert isinstance(position, int)
+        assert isinstance(nodes, list)
+        assert isinstance(parent, QModelIndex)
+        for node in nodes:
+            assert isinstance(node, Node)
+
+        model = self
+        node = model.getNode(parent)
+
+        self.beginInsertRows(parent, position, position + len(nodes) - 1)
+
+        for child in reversed(nodes):
+            success = node.insert_child(position, child)
+
+        self.endInsertRows()
+
+        return True
 
 class Units:
     def __init__(self):
